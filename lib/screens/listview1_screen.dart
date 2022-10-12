5102ecb038d0a4c3f9a5c93eae7fd4c4a311c343
David Forero <davidforero717@gmail.com>
2022-10-11 22:46:25 -0500

    se usa Listview y se muestra una lista de elementos

---
+++ lib/screens/listview1_screen.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Listview1Screen extends StatelessWidget {
+  const Listview1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Listview tipo 1'),
+      ),
+      body: ListView(
+        children: const [
+          Text('Hola mundo'),
+          Text('Hola mundo'),
+          Text('Hola mundo'),
+          Text('Hola mundo'),
+          Text('Hola mundo'),
+        ],
+      ),
+    );
+  }
+}
