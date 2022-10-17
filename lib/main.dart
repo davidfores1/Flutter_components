28b956694866dc9319876fec18d817e4f9c026e4
David Forero <david.forero@supergiros.com.co>
2022-10-17 14:17:15 -0500

    prueba de nuevo colaborador al proyecto

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home:AlertScreen(),
+      home:Card(),
 
     );
   }
