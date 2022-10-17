352bc8592b08207e368912b92bf443f21b4bc85f
David Forero <david.forero@supergiros.com.co>
2022-10-17 16:40:22 -0500

    se realiza diseño en el home_screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -7,17 +7,22 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Material App',
-      home: Scaffold(
+    return  Scaffold(
         appBar: AppBar(
-          title: const Text('Material App Bar'),
+          title: const Text('Componentes en Flutter'),
+          elevation: 0,
         ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
+        body: ListView.separated(
+          itemBuilder: (context, index) =>ListTile(
+            leading: const Icon(Icons.access_time_filled_outlined),
+            title: const Text('Nombre de ruta'),
+            onTap:(){
+
+            } 
+          ),
+          separatorBuilder: (_, index) => const Divider(),
+          itemCount: 10,
+        ), 
+      );
   }
 }
