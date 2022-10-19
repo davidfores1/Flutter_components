bff3dd3acc0379626579778bca581ee7f3133aef
David Forero <david.forero@supergiros.com.co>
2022-10-19 09:24:42 -0500

    se utiliza el card widget

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/theme/app_theme.dart';
 
 class CardScreen extends StatelessWidget {
    
@@ -6,9 +7,25 @@ class CardScreen extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('CardScreen'),
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('Card Widget'),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+         children: [
+          Card(
+            child: Column(
+              children: const [
+                ListTile(
+                  leading:  Icon(Icons.photo_album_outlined, color: AppTheme.primary),
+                  title: Text('Soy un titulo'),
+                  subtitle: Text('StrutStyle? strutStyle, TextAlign? textAlign, TextDirection? textDirection, Locale? locale, bool? softWrap, TextOverflow? overflow, double? textScaleFactor, int? maxLines, String? semanticsLabel, TextWidthBasis? textWidthBasis, TextHeightBehavior? textHeightBehavior, Color? selectionColor'),
+                )
+              ],
+            ),
+          )
+         ],
       ),
     );
   }
