8aa60457cf71a84aeed2768eaeab66c39b80ffda
David Forero <davidforero717@gmail.com>
2022-10-20 07:43:05 -0500

    se realiza argumentos en los widget

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -3,22 +3,35 @@ import 'package:flutter/material.dart';
 import '../widgets/widgets.dart';
 
 class CardScreen extends StatelessWidget {
-   
   const CardScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: const Text('Card Widget'),
       ),
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-         children: const[
+        children: const [
           CustomCard1(),
           SizedBox(height: 10),
-          CustomCard2(),
-         ],
+          CustomCard2(
+              name:'¡Me voy de viaje!',
+              imageUrl:
+                  'https://www.lavanguardia.com/files/og_thumbnail/uploads/2018/06/15/5fa43d71a111f.jpeg'),
+          SizedBox(height: 10),
+          CustomCard2(
+            name:'¡LLegue a mi destino!', 
+            imageUrl:
+                'https://growproexperience.com/_next/image/?url=https%3A%2F%2Fwp.growproexperience.com%2Fwp-content%2Fuploads%2F2022%2F06%2Fviajes-para-aprender-ingles.jpg&w=1920&q=75',
+          ),
+          SizedBox(height: 10),
+          CustomCard2(
+              imageUrl:
+                  'https://thelandscapephotoguy.com/wp-content/uploads/2019/01/landscape%20new%20zealand%20S-shape.jpg'),
+          SizedBox(height: 10),
+        ],
       ),
     );
   }
--- lib/widgets/custom_card2.dart
+++ lib/widgets/custom_card2.dart
@@ -1,16 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/theme/app_theme.dart';
 
 class CustomCard2 extends StatelessWidget {
-  const CustomCard2({super.key});
+
+  final String imageUrl;
+  final String? name;
+
+  const CustomCard2({
+    super.key, 
+    required this.imageUrl, this.name});
 
   @override
   Widget build(BuildContext context) {
     return Card(
+      clipBehavior: Clip.antiAlias,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15)
+      ),
       elevation: 10,
+      shadowColor: AppTheme.primary,
        child: Column(
-        children: const [
-          FadeInImage(
-            image: NetworkImage('https://thelandscapephotoguy.com/wp-content/uploads/2019/01/landscape%20new%20zealand%20S-shape.jpg'), placeholder: AssetImage('assets/jar-loading.gif'),
+        children: [
+           FadeInImage(
+            image: NetworkImage(imageUrl), 
+            placeholder: const AssetImage('assets/jar-loading.gif'),
+            width: double.infinity,
+            height: 230,
+            fit:BoxFit.cover,
+            fadeInDuration: const Duration(milliseconds: 300),
+            ),
+            if(name != null)
+            Container(
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(right: 20, top:10, bottom:10),
+              child: Text(name ?? 'No TiTle'),
             )
         ],
        ),
