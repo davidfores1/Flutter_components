f953c2592e4807743dba2a3d3283829c425b6f87
David Forero <davidforero717@gmail.com>
2023-02-08 16:57:16 -0500

    se realiza  checkbox y switches

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_components/theme/app_theme.dart';
 
 class SliderScreen extends StatefulWidget {
    
@@ -9,34 +10,77 @@ class SliderScreen extends StatefulWidget {
 }
 
 class _SliderScreenState extends State<SliderScreen> {
+  
   double _SliderValue = 100;
+  bool _SliderEnable = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Sliders & Checks'),
       ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            Slider.adaptive(
-              min: 50,
-              max: 400,
-              value: _SliderValue,
+      body: Column(
+        children: [
+          Slider.adaptive(
+            min: 50,
+            max: 400,
+            value: _SliderValue,
+            onChanged: _SliderEnable ? (value){
+             _SliderValue = value;
+             setState(() {});
+            }
+            :null
+            ),
+
+            Checkbox(
+              value: _SliderEnable, 
               onChanged: (value){
-               _SliderValue = value;
+               _SliderEnable = value ?? true;
                setState(() {
-          
+                 
                });
               }
+            ),
+
+            Switch(
+              value: _SliderEnable,
+              onChanged:(value) => setState(() {
+                _SliderEnable = value;
+              }),
+            ),
+
+            CheckboxListTile(
+              activeColor: AppTheme.primary,
+              title: const Text('Habilitar Slider'),
+              value: _SliderEnable, 
+              onChanged:(value) => setState(() {
+                _SliderEnable = value ?? true;
+              }),
+            ),
+
+              SwitchListTile.adaptive(
+              activeColor: AppTheme.primary,
+              title: const Text('Habilitar Slider'),
+              value: _SliderEnable, 
+              onChanged:(value) => setState(() {
+                _SliderEnable = value;
+              }),
+            ),
+            
+            //Ver licencia que la aplicacion esta utilizando
+            const AboutListTile(),
+
+            Expanded(
+              child: SingleChildScrollView(
+                child: Image(
+                  image: const NetworkImage('https://www.pngarts.com/files/2/Batman-PNG-Picture.png'),
+                  fit: BoxFit.contain,
+                  width: _SliderValue,
+                  ),
               ),
-              Image(
-                image: NetworkImage('https://www.pngarts.com/files/2/Batman-PNG-Picture.png'),
-                fit: BoxFit.contain,
-                width: _SliderValue,
-                )
-          ],
-        ),
+            )
+        ],
       )
     );
   }
