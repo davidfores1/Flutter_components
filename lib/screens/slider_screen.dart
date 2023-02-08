f37ba71ebd7dead27733b2dfc0f71cd4b12b29c1
David Forero <davidforero717@gmail.com>
2023-02-08 08:31:33 -0500

    se realiza el widget slider

--- lib/screens/slider_screen.dart
+++ lib/screens/slider_screen.dart
@@ -1,15 +1,43 @@
 import 'package:flutter/material.dart';
 
-class SliderScreen extends StatelessWidget {
+class SliderScreen extends StatefulWidget {
    
   const SliderScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<SliderScreen> createState() => _SliderScreenState();
+}
+
+class _SliderScreenState extends State<SliderScreen> {
+  double _SliderValue = 100;
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('SliderScreen'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sliders & Checks'),
       ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Slider.adaptive(
+              min: 50,
+              max: 400,
+              value: _SliderValue,
+              onChanged: (value){
+               _SliderValue = value;
+               setState(() {
+          
+               });
+              }
+              ),
+              Image(
+                image: NetworkImage('https://www.pngarts.com/files/2/Batman-PNG-Picture.png'),
+                fit: BoxFit.contain,
+                width: _SliderValue,
+                )
+          ],
+        ),
+      )
     );
   }
 }
